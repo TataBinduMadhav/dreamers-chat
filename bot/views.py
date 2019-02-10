1f12e1913db6644da46432466909f7c835c15048
Bindu Madhav Tata <bindumadhavtata@gmail.com>
2019-02-10 22:12:51 +0530

    Initial Commit

---
+++ bot/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def bview(request):
+    if request.GET['hub.verify_token'] == 'YOUR_SECRET_TOKEN':
+        return HttpResponse(request.GET['hub.challenge'])
+    else :
+        return HttpResponse('Invalid Token !')
